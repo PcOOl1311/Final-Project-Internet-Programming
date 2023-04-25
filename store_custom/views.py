5ee91e806921247131c47866a374c4d45f91ffce
PcOOl1311 <kostas13vr1@gmail.com>
2023-04-25 12:26:37 +0300

    bug fixxes

--- store_custom/views.py
+++ store_custom/views.py
@@ -2,7 +2,6 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
-from django.core.checks import messages
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import redirect, render
